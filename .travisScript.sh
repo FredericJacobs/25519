d07c6312d3148745368eca1b223bafd216557b04
Frederic Jacobs <github@fredericjacobs.com>
2014-07-25 15:54:07 -1000

    Codesigning

---
+++ .travisScript.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+KEY_CHAIN=ios-build.keychain
+security create-keychain -p travis $KEY_CHAIN
+# Make the keychain the default so identities are found
+security default-keychain -s $KEY_CHAIN
+# Unlock the keychain
+security unlock-keychain -p travis $KEY_CHAIN
+# Set keychain locking timeout to 3600 seconds
+security set-keychain-settings -t 3600 -u $KEY_CHAIN
