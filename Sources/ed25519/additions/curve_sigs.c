ae1993b4ca69ef00d8d87c0e278c7a2bca76ccb9
Frederic Jacobs <github@fredericjacobs.com>
2014-09-21 13:17:28 +0200

    Pulling Trevor's changes to the point conversion.

--- Sources/ed25519/additions/curve_sigs.c
+++ Sources/ed25519/additions/curve_sigs.c
@@ -110,6 +110,7 @@ int curve25519_verify(const unsigned char* signature,
   /* The below call has a strange API: */
   /* verifybuf = R || S || message */
   /* verifybuf2 = internal to next call gets a copy of verifybuf, S gets 
-     replaced with pubkey for hashing, then the whole thing gets zeroized */
+     replaced with pubkey for hashing, then the whole thing gets zeroized
+     (if bad sig), or contains a copy of msg (good sig) */
   return crypto_sign_open(verifybuf2, &some_retval, verifybuf, 64 + msg_len, ed_pubkey);
 }
