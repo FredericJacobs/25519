93ae044c4064b1adbdacfa1061e728f8246092de
Igor Ranieri Elland <igor@elland.me>
2016-11-17 10:02:51 +0100

    Add shared framework for Carthage support.

---
+++ Curve25519/Curve25519.h
@@ -0,0 +1,21 @@
+//
+//  Curve25519.h
+//  Curve25519
+//
+//  Created by Igor Ranieri on 17/11/2016.
+//  Copyright © 2016 Whisper Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Security/Security.h>
+#include <stdio.h>
+
+//! Project version number for Curve25519.
+FOUNDATION_EXPORT double Curve25519VersionNumber;
+
+//! Project version string for Curve25519.
+FOUNDATION_EXPORT const unsigned char Curve25519VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Curve25519/PublicHeader.h>
+
+
